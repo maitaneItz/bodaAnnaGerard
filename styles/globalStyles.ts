87934d7d8d8d4528f30c8a50e3531631d54d2358
maitaneItz <maitanei@hotmail.com>
2021-03-08 19:43:01 +0100

    Feat: añade imágenes

--- styles/globalStyles.ts
+++
@@ -1,8 +0,0 @@
-import { createGlobalStyle } from "styled-components";
-import * as tokens from "../dictionary/variables";
-
-export const GlobalStyles = createGlobalStyle`
-  * {
-    color: ${tokens.FontColor};
-  }
-`;
