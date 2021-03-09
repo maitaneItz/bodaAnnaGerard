f938cfd4dc9673750cf0f52b1d4302578d69f7ef
Kike Arbunies <kikearbunies@gmail.com>
2021-03-09 17:15:52 +0100

    Feat: fix header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,12 +1,12 @@
 import { Container } from "../Container";
-import { Wrapper, ImageTop, Date, InnerWrapper } from "./styles";
+import { Wrapper, ImageTop, Date, InnerWrapper, Title } from "./styles";
 
 export const Header = () => (
   <Wrapper>
     <Container>
       <ImageTop src="/images/hojasHeader.svg" />
       <InnerWrapper>
-        <img src="/images/name.svg" />
+        <Title src="/images/name.svg" />
         <Date>22 Julio</Date>
       </InnerWrapper>
     </Container>
